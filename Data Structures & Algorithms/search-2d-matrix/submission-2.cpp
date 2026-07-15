cef212891268d760af365568942aa6565a3ac8d1
Kiran Rajeev <145009677+KiranRajeev-KV@users.noreply.github.com>
2026-07-15 15:19:27 +0530

    Add: search-2d-matrix - submission-2

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        if (matrix.empty() || matrix[0].empty()) {
+            return false;
+        }
+
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int l = 0;
+        int r = (n*m)-1;
+
+        while(l<=r) {
+            int mid = l+(r-l)/2;
+            int row = mid/n;
+            int col = mid%n;
+
+            if(matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] < target) {
+                l = mid +1;
+            } else if (matrix[row][col] > target) {
+                r = mid -1;
+            }
+        }
+        return false;
+    }
+};
